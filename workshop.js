1460b037d9ce33246956557786b7ebea60dd36f9
Ben Beatty <benbeatty85@gmail.com>
2017-07-04 22:21:48 +0000

    finished some function

--- workshop.js
+++ workshop.js
@@ -50,7 +50,16 @@ function every(predicate, theArray) {
 }
 
 function some(predicate, theArray) {
-
+  if(theArray.length === 0) {
+    return false;
+  }
+    for (var i = 0; i < theArray.length; i ++) {
+      if (predicate(theArray[i])) {
+        return true;
+      }
+    
+  }
+  return false;
 }
 
 function indexOf(item, theArray) {
